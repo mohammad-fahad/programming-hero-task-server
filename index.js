3a204266a43267483c0dc16c7287f3d44e93f432
Mohammad Fahad <mail.maohammadfahad@gmail.com>
2020-12-20 20:58:04 +0600

    done

--- index.js
+++ index.js
@@ -76,6 +76,4 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(8000, () => {
-    console.log('Example app listening at http://localhost:8000')
-})
+app.listen(process.env.PORT || 4200);
